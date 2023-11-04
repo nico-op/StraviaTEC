8e0128e3a56538187abe5ac3074b54f49b03fbf1
Gabriel Lobo <kevinlobo1920k@gmail.com>
2023-11-04 03:26:52 -0600

    configuración inicial del api

---
+++ StraviaWebApi/Startup.cs
@@ -0,0 +1,72 @@
+using Microsoft.OpenApi.Models;
+
+
+namespace StraviaWwebApi
+{
+    public class Startup
+    {
+        private readonly string _Cors = "_Cors";
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddControllersWithViews();
+            services.AddEndpointsApiExplorer();
+
+            services.AddCors(options => {
+                options.AddPolicy(name: _Cors, builder =>
+                {
+                    builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+                    .AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "StraviaTEC API", Version = "v1" });
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseCors(_Cors);
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "StraviaTEC API ");
+        
+            });
+            
+
+            // Configura la canalización de solicitud HTTP aquí
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                //Ruta de los controladores
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
