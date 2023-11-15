0b43118d91d5ccc9bd69e0e6af4600e2a8e06eb2
Gabriel Lobo <kevinlobo1920k@gmail.com>
2023-11-15 16:29:59 -0600

    cambios en la estructura del codigo para mongodb

--- StraviaWApiMongo/Controllers/ComentarioController.cs
+++ StraviaWApiMongo/Controllers/ComentarioController.cs
@@ -8,7 +8,7 @@ using StraviaWApiMongo.Services;
 
 namespace StraviaWApiMongo.Controllers;
 
-[Route("api/[controller]")]
+[Route("/")]
 [ApiController]
 public class ComentarioController : ControllerBase
 {
