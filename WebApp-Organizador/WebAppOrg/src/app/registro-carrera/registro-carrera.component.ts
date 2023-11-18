746fe6ac2cb166a488cd6456d4372641909d2bcf
Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>
2023-11-18 15:25:32 -0600

    Registro Carrera

--- WebApp-Organizador/WebAppOrg/src/app/registro-carrera/registro-carrera.component.ts
+++ WebApp-Organizador/WebAppOrg/src/app/registro-carrera/registro-carrera.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-registro-carrera',
-  templateUrl: './registro-carrera.component.html',
-  styleUrls: ['./registro-carrera.component.css']
+    selector: 'app-registro-carrera',
+    templateUrl: './registro-carrera.component.html',
+    styleUrls: ['./registro-carrera.component.css']
 })
 export class RegistroCarreraComponent {
+  
 
 }
