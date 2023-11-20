00d891517a3c2df99721686bc12ae11f89b1c9b0
Gabriel Lobo <kevinlobo1920k@gmail.com>
2023-11-19 20:12:44 -0600

    cambios en el trigger

--- Script/ScriptTriggers.sql
+++ Script/ScriptTriggers.sql
@@ -15,23 +15,22 @@ BEGIN
 END;
 GO
 
--- Trigger: EvitarUsuariosReplicados
--- Función: Evita que al momento de hacer un insert de Usuario, tenga un NombreUsuario replicado
+-- Trigger: ValidarFechaCarrera
+-- Función: Evita que la fecha sea antes de la fecha actual, es decir, no se puede crear una carrera en el pasado.
 
-CREATE TRIGGER EvitarUsuariosReplicados
-ON Usuario
-AFTER INSERT
+CREATE OR ALTER TRIGGER ValidarFechaCarrera
+ON Carrera
+AFTER INSERT, UPDATE
 AS
 BEGIN
     IF EXISTS (
-        SELECT 1
-        FROM inserted I
-        INNER JOIN Usuario U ON U.NombreUsuario = I.NombreUsuario
+        SELECT * 
+        FROM inserted 
+        WHERE FechaCarrera < GETDATE()
     )
     BEGIN
-        RAISERROR ('El usuario ya existe en la tabla.', 16, 1)
-        ROLLBACK TRANSACTION
-        RETURN
+        RAISERROR('La fecha de la carrera no puede ser anterior a la fecha actual.', 16, 1)
+        ROLLBACK TRANSACTION; -- Deshacer la transacción para evitar la inserción o actualización
     END
 END;
 GO
@@ -48,11 +47,11 @@ BEGIN
     DECLARE @FechaNacimiento DATE, @FechaActual DATETIME;
     SELECT @FechaNacimiento = Fecha_nacimiento, @FechaActual = Fecha_actual FROM inserted;
 
-    IF DATEDIFF(YEAR, @FechaNacimiento, @FechaActual) < 10
+    IF DATEDIFF(YEAR, @FechaNacimiento, @FechaActual) < 5
     BEGIN
         RAISERROR ('La diferencia entre la fecha de nacimiento y la fecha actual debe ser de al menos 10 años.', 16, 1);
         ROLLBACK TRANSACTION;
         RETURN;
     END
 END;
-
+GO
