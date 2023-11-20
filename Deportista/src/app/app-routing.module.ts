b90eee7f6e403d999caee5f76ead2603bb4dedc2
noelz8 <ncastroa@estudianTEC.cr>
2023-11-19 23:11:15 -0600

    paths
    a

--- Deportista/src/app/app-routing.module.ts
+++ Deportista/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActividadComponent } from './Pages/actividad/actividad.component';
+import { BuscarUsuarioComponent } from './Pages/buscar-usuario/buscar-usuario.component';
+import { CrearActividadComponent } from './Pages/crear-actividad/crear-actividad.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegistrarComponent } from './Pages/registrar/registrar.component';
 
 
-const routes: Routes = [ ];
+const routes: Routes = [ 
+  {path: '', component: InicioComponent},
+  {path: 'actividad', component: ActividadComponent},
+  {path: 'buscar-usuario', component: BuscarUsuarioComponent},
+  {path: 'crear-actividad', component: CrearActividadComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registrar', component: RegistrarComponent},
+  {path: '**', redirectTo: 'registrar', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
